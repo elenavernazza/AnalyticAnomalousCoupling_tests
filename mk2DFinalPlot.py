0c98562c777a8788e906fd245c9f766ef77daeec
Giacomo Boldrini <gboldrin@cern.ch>
2021-02-01 19:49:43 +0100

    New tools for 2D plotting

--- mk2DFinalPlot.py
+++ mk2DFinalPlot.py
@@ -12,6 +12,33 @@ from array import array
 import math
 from collections import OrderedDict
 
+def ConvertOptoLatex(op):
+
+    d = {
+        'cHDD': 'Q_{HD}',
+        'cHbox': 'Q_{H#Box}',
+        'cW': 'Q_{W}',
+        'cHB': 'Q_{HB}',
+        'cHW': 'Q_{HW}',
+        'cHWB': 'Q_{HWB}',
+        'cll': 'Q_{ll}',
+        'cll1': 'Q_{ll}\'',
+        'cqq1': 'Q_{qq}^{(1)}',
+        'cqq11': 'Q_{qq}^{(1)}\'',
+        'cHl1': 'Q_{Hl}^{(1)}',
+        'cHl3': 'Q_{Hl}^{(3)}',
+        'cHq1': 'Q_{Hq}^{(1)}',
+        'cHq3': 'Q_{Hq}^{(3)}',
+        'cHe': 'Q_{He}',
+        'cHu': 'Q_{Hu}',
+        'cHd': 'Q_{Hd}',
+        'cqq3': 'Q_{qq}^{(3)}',
+        'cqq31': 'Q_{qq}^{(3)}\'',
+
+    }
+
+    return d[op]
+
 def Retrieve2DLikelihood(file, op, maxNLL, xscale, yscale):
 
     f = ROOT.TFile(file)
@@ -43,8 +70,8 @@ def Retrieve2DLikelihood(file, op, maxNLL, xscale, yscale):
 
     graphScan = ROOT.TGraph2D(n,x,y,z)
 
-    #graphScan.SetNpx(400)
-    #graphScan.SetNpy(400)
+    graphScan.SetNpx(100)
+    graphScan.SetNpy(100)
 
     graphScan.GetZaxis().SetRangeUser(0, float(maxNLL))
     graphScan.GetHistogram().GetZaxis().SetRangeUser(0, float(maxNLL))
@@ -83,6 +110,7 @@ if __name__ == "__main__":
     parser.add_argument('--lumi',     dest='lumi',     help='lumi to plot', required = False, default = "100")
     args = parser.parse_args()
 
+    is_combo = False
     operators = OrderedDict()
     plt_options = {}
 
@@ -91,49 +119,148 @@ if __name__ == "__main__":
 
     mkdir(args.outf)
 
-    for op in operators.keys():
-        canvas_d = {
-            '1sg' : [],
-            '2sg' : [],
-            'min' : [],
-            'names' : [],
-            'base_op' : op,
-            'n_op' : [],
-            'scale' : [],
-        }
+    if not is_combo:
 
-        mkdir(args.outf + "/" + op)
+        for op in operators.keys():
+            canvas_d = {
+                '1sg' : [],
+                '2sg' : [],
+                'min' : [],
+                'names' : [],
+                'base_op' : op,
+                'n_op' : [],
+                'scale' : [],
+            }
 
-        for proc in operators[op].keys():
-            gs, cont_graphs, exp = Retrieve2DLikelihood(operators[op][proc]['path'], 
-                                                              [operators[op][proc]['op'],op], args.maxNLL, 
-                                                                    float(operators[op][proc]['xscale']), float(operators[op][proc]['yscale']) )
-            
-            print(operators[op][proc]['op'],op)
-            color = operators[op][proc]['linecolor']
-            size = operators[op][proc]['linesize']
+            mkdir(args.outf + "/" + op)
 
-            cont_graphs[0].SetLineWidth(size)
-            #cont_graphs[1].SetLineWidth(size)
+            for proc in operators[op].keys():
+                gs, cont_graphs, exp = Retrieve2DLikelihood(operators[op][proc]['path'], 
+                                                                [operators[op][proc]['op'],op], args.maxNLL, 
+                                                                        float(operators[op][proc]['xscale']), float(operators[op][proc]['yscale']) )
+                
+                print(operators[op][proc]['op'],op)
+                color = operators[op][proc]['linecolor']
+                size = operators[op][proc]['linesize']
 
-            cont_graphs[0].SetLineColor(color)
-            #cont_graphs[1].SetLineColor(color)
+                cont_graphs[0].SetLineWidth(size)
 
-
-            canvas_d['1sg'].append(cont_graphs[0])
-            #canvas_d['2sg'].append(cont_graphs[1])
-            canvas_d['min'].append(exp)
-            canvas_d['names'].append(operators[op][proc]['name'])
-            canvas_d['n_op'].append(operators[op][proc]['op'])
-            canvas_d['scale'].append([float(operators[op][proc]['xscale']), float(operators[op][proc]['yscale'])])
-
-        #create as many canvas as necessary each containing up to 5 contours
-        n_ = math.ceil(float(len(canvas_d['1sg']))/5)
+                cont_graphs[0].SetLineColor(color)
+                #x_min, x_max, y_min, y_max = cont_graphs[0].GetXaxis().GetXmin(), cont_graphs[0].GetXaxis().GetXmax(), cont_graphs[0].GetYaxis().GetXmin(), cont_graphs[0].GetYaxis().GetXmax()
 
 
-        for idx in range(0,int(n_)):
+                canvas_d['1sg'].append(cont_graphs[0])
+                canvas_d['min'].append(exp)
+                canvas_d['names'].append(operators[op][proc]['name'])
+                canvas_d['n_op'].append(operators[op][proc]['op'])
+                canvas_d['scale'].append([float(operators[op][proc]['xscale']), float(operators[op][proc]['yscale'])])
 
-            c = ROOT.TCanvas("c_{}_{}".format(op, idx), "c_{}_{}".format(op, idx), 1000, 1000)
+            #create as many canvas as necessary each containing up to 5 contours
+            n_ = math.ceil(float(len(canvas_d['1sg']))/5)
+
+
+            for idx in range(0,int(n_)):
+
+                c = ROOT.TCanvas("c_{}_{}".format(op, idx), "c_{}_{}".format(op, idx), 1000, 1000)
+
+                margins = 0.13
+
+                ROOT.gPad.SetRightMargin(margins)
+                ROOT.gPad.SetLeftMargin(margins)
+                ROOT.gPad.SetBottomMargin(margins)
+                ROOT.gPad.SetTopMargin(margins)
+                ROOT.gPad.SetFrameLineWidth(3)
+                ROOT.gPad.SetTicks()
+
+                leg = ROOT.TLegend(0.15, 0.8, 0.85, 0.85)
+                leg.SetBorderSize(0)
+                leg.SetNColumns(len(canvas_d['1sg'][ idx*5: (idx*5) + 5]))
+                leg.SetTextSize(0.025)
+
+                linestyles = [1,2,3,5,6] * int(n_)
+                
+
+                c.SetGrid()
+                y_min_new = canvas_d['1sg'][idx*5].GetYaxis().GetXmin() - abs(0.2*canvas_d['1sg'][idx*5].GetYaxis().GetXmin())
+                y_max_new = canvas_d['1sg'][idx*5].GetYaxis().GetXmax() + abs(0.2*canvas_d['1sg'][idx*5].GetYaxis().GetXmax()) 
+                canvas_d['1sg'][idx*5].GetYaxis().SetRangeUser(y_min_new, y_max_new)
+                canvas_d['1sg'][idx*5].GetYaxis().SetTitleOffset(1.5)
+                canvas_d['1sg'][idx*5].GetYaxis().SetTitle(op)
+                canvas_d['1sg'][idx*5].GetXaxis().SetTitle("2nd Operator")
+                canvas_d['1sg'][idx*5].SetTitle("")
+                canvas_d['1sg'][idx*5].SetLineStyle(linestyles[0])
+                canvas_d['1sg'][idx*5].Draw("AL")
+                canvas_d['min'][idx*5].Draw("P")
+
+                name = canvas_d['n_op'][idx*5]
+                if canvas_d['scale'][idx*5][0] != 1: name =  str(canvas_d['scale'][idx*5][0]) + " #times " + name
+
+                leg.AddEntry(canvas_d['1sg'][idx*5], name, "L")
+
+                for i,j, n, ls, scale in zip(canvas_d['1sg'][idx*5 +1:  (idx*5) + 5], canvas_d['min'][idx*5 +1:  (idx*5) + 5], canvas_d['n_op'][idx*5 +1:  (idx*5) + 5], linestyles[idx*5 +1:  (idx*5) + 5], canvas_d['scale'][idx*5 +1:  (idx*5) + 5]):
+                    i.SetLineStyle(ls)
+                    i.Draw("L same")
+                    j.Draw("P same")
+                    name = n 
+                    if scale[0]!=1 : name =  str(scale[0]) + " #times " + n
+                    leg.AddEntry(i, name, "L")
+
+                #Draw fancy
+
+                tex3 = ROOT.TLatex(0.86,.89,"100 fb^{-1}   (13 TeV)")
+                tex3.SetNDC()
+                tex3.SetTextAlign(31)
+                tex3.SetTextFont(42)
+                tex3.SetTextSize(0.04)
+                tex3.SetLineWidth(2)
+                tex3.Draw()
+
+                if "process" in plt_options.keys():
+                    if 'xpos' not in  plt_options.keys(): xpos = 0.35
+                    else: xpos = plt_options['xpos']
+                    if 'size' not in  plt_options.keys(): size = 0.04
+                    else: size = plt_options['size']
+                    tex4 = ROOT.TLatex(xpos,.89,plt_options['process'])
+                    tex4.SetNDC()
+                    tex4.SetTextAlign(31)
+                    tex4.SetTextFont(42)
+                    tex4.SetTextSize(size)
+                    tex4.SetLineWidth(2)
+                    tex4.Draw()
+
+                leg.Draw()
+                c.Draw()
+                c.Print(args.outf + "/" + op + "/" + op + "{}.pdf".format(idx))
+
+        
+    else:
+
+        for op_pair in operators.keys():
+
+            mkdir(args.outf + "/" + op_pair)
+
+            ops = op_pair.split("_")
+            op_x = ops[0]
+            op_y = ops[1]
+
+            channels = operators[op_pair].keys()
+
+
+            graphs = []
+
+            for key in channels:
+                gs, cont_graphs, exp = Retrieve2DLikelihood(operators[op_pair][key]['path'], 
+                                                                [op_x, op_y], args.maxNLL, 
+                                                                        1., 1. )
+
+                cont_graphs[0].SetLineWidth(3)
+
+                cont_graphs[0].SetLineColor(int(operators[op_pair][key]['color']))
+                cont_graphs[0].SetLineStyle(int(operators[op_pair][key]['linestyle']))
+
+                graphs.append([key, cont_graphs[0], exp])
+
+            c = ROOT.TCanvas("c_{}".format(op_pair), "c_{}".format(op_pair), 1000, 1000)
 
             margins = 0.13
 
@@ -146,36 +273,29 @@ if __name__ == "__main__":
 
             leg = ROOT.TLegend(0.15, 0.8, 0.85, 0.85)
             leg.SetBorderSize(0)
-            leg.SetNColumns(len(canvas_d['1sg'][ idx*5: (idx*5) + 5]))
+            leg.SetNColumns(len(graphs))
             leg.SetTextSize(0.025)
 
-            linestyles = [1,2,3,5,6] * int(n_)
-            
-
             c.SetGrid()
-            y_min_new = canvas_d['1sg'][idx*5].GetYaxis().GetXmin() - abs(0.2*canvas_d['1sg'][idx*5].GetYaxis().GetXmin())
-            y_max_new = canvas_d['1sg'][idx*5].GetYaxis().GetXmax() + abs(0.2*canvas_d['1sg'][idx*5].GetYaxis().GetXmax()) 
-            canvas_d['1sg'][idx*5].GetYaxis().SetRangeUser(y_min_new, y_max_new)
-            canvas_d['1sg'][idx*5].GetYaxis().SetTitleOffset(1.5)
-            canvas_d['1sg'][idx*5].GetYaxis().SetTitle(op)
-            canvas_d['1sg'][idx*5].GetXaxis().SetTitle("2nd Operator")
-            canvas_d['1sg'][idx*5].SetTitle("")
-            canvas_d['1sg'][idx*5].SetLineStyle(linestyles[0])
-            canvas_d['1sg'][idx*5].Draw("AL")
-            canvas_d['min'][idx*5].Draw("P")
 
-            name = canvas_d['n_op'][idx*5]
-            if canvas_d['scale'][idx*5][0] != 1: name =  str(canvas_d['scale'][idx*5][0]) + " #times " + name
+            #first graph
+            graphs[0][1].GetYaxis().SetTitleOffset(1.5)
+            graphs[0][1].GetYaxis().SetTitle(ConvertOptoLatex(op_y))
+            graphs[0][1].GetXaxis().SetTitle(ConvertOptoLatex(op_x))
+            graphs[0][1].SetTitle("")
+            #graphs[0][1].SetLineStyle(linestyles[0])
+            graphs[0][1].Draw("AL")
+            graphs[0][2].Draw("P same")
 
-            leg.AddEntry(canvas_d['1sg'][idx*5], name, "L")
+            name = graphs[0][0]
+            leg.AddEntry(graphs[0][1], name, "L")
 
-            for i,j, n, ls, scale in zip(canvas_d['1sg'][idx*5 +1:  (idx*5) + 5], canvas_d['min'][idx*5 +1:  (idx*5) + 5], canvas_d['n_op'][idx*5 +1:  (idx*5) + 5], linestyles[idx*5 +1:  (idx*5) + 5], canvas_d['scale'][idx*5 +1:  (idx*5) + 5]):
-                i.SetLineStyle(ls)
-                i.Draw("L same")
-                j.Draw("P same")
-                name = n 
-                if scale[0]!=1 : name =  str(scale[0]) + " #times " + n
-                leg.AddEntry(i, name, "L")
+            for i in graphs[1:]:
+                    i[1].Draw("L same")
+                    i[2].Draw("P same")
+                    name = i[0]
+                    #if scale[0]!=1 : name =  str(scale[0]) + " #times " + n
+                    leg.AddEntry(i[1], name, "L")
 
             #Draw fancy
 
@@ -190,18 +310,17 @@ if __name__ == "__main__":
             if "process" in plt_options.keys():
                 if 'xpos' not in  plt_options.keys(): xpos = 0.35
                 else: xpos = plt_options['xpos']
-                tex4 = ROOT.TLatex(0.35,.89,plt_options['process'])
+                if 'size' not in  plt_options.keys(): size = 0.04
+                else: size = plt_options['size']
+                tex4 = ROOT.TLatex(xpos,.89,plt_options['process'])
                 tex4.SetNDC()
                 tex4.SetTextAlign(31)
                 tex4.SetTextFont(42)
-                tex4.SetTextSize(0.04)
+                tex4.SetTextSize(size)
                 tex4.SetLineWidth(2)
                 tex4.Draw()
 
             leg.Draw()
             c.Draw()
-            c.Print(args.outf + "/" + op + "/" + op + "{}.pdf".format(idx))
-
-        
-
+            c.Print(args.outf + "/" + op_pair + "/" + "_".join(i for i in channels) + ".pdf")
 
---
+++ mkCombinationConfig2D.py
@@ -0,0 +1,145 @@
+#python mkCombinationConfig2D.py --combo /afs/cern.ch/work/g/gboldrin/public/public/CMSSW_10_2_13/src/HiggsAnalysis/AnalyticAnomalousCoupling/test/SSWW_OSWW_2op --prefix combination_SSWW_OSWW --process SSWW_OSWW --ch /afs/cern.ch/work/g/gboldrin/public/public/CMSSW_10_2_13/src/HiggsAnalysis/AnalyticAnomalousCoupling/test/SSWW_2op_best,/afs/cern.ch/work/g/gboldrin/public/public/CMSSW_10_2_13/src/HiggsAnalysis/AnalyticAnomalousCoupling/test/OSWW_2op_best --chprefix to_Latinos,to_Latinos --chprocess SSWW,OSWW --model EFTNeg --out 2DPlotConfig/SSWW_OSWW_2D.py
+
+from glob import glob 
+import os 
+import argparse 
+import ROOT
+import sys
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Command line parser for 2D plotting scans')
+    parser.add_argument('--combo',     dest='combo',     help='target folder with combined fits', required = True)
+    parser.add_argument('--prefix',     dest='prefix',     help='target folder prefix', required = True)
+    parser.add_argument('--process',     dest='process',     help='target folder process name', required = True)
+
+    parser.add_argument('--ch',     dest='ch',     help='comma separated list target folder with channel fits', required = True)
+    parser.add_argument('--chprefix',     dest='chprefix',     help='comma separated list channel folder prefix', required = True)
+    parser.add_argument('--chprocess',     dest='chprocess',     help='comma separated list channel folder process name', required = True)
+
+    parser.add_argument('--model',     dest='model',     help='target folder model name', required = True)
+
+    parser.add_argument('--colors',     dest='colors',     help='Colors for each channel, last one is the combination', required = False, default = "870,797,829,909,1")
+    parser.add_argument('--linestyles',     dest='linestyles',     help='Linestyle for each channel, last one is the combination', required = False, default = "2,3,5,6,1")
+
+    parser.add_argument('--out',     dest='out',     help='output name', required = True)
+    parser.add_argument('--verbose',     dest='verbose',     help='verbosity', required = False, default = False, action = "store_true")
+    args = parser.parse_args()
+
+
+    #splitting channels lists
+    ch_ = args.ch.split(",")
+    chp_ = args.chprefix.split(",")
+    chprocess_ =args.chprocess.split(",")
+    colors = [int(i) for i in args.colors.split(",")]
+    lines_ = [int(i) for i in args.linestyles.split(",")]
+
+
+    # full 15 operators of interest
+    ops = ['cqq3', 'cqq31', 'cHl3', 'cqq1', 'cqq11', 'cll1', 'cW', 'cHq3', 'cHbox', 'cHq1', 'cHW', 'cHWB', 'cHDD', 'cHl1', 'cll']
+
+    # final dict will contain: 
+    # op combo as key
+    # combination path to fit
+    # for all channels, path to their fit
+    final_dict = {}
+
+    for op in ops:
+        
+        ls  = glob(args.combo+ "/" + args.prefix + "_" + op + "_*") 
+
+        for path in ls:
+
+            subop = path.split(args.combo+ "/" + args.prefix + "_")[1]
+            final_dict[subop] = {}
+
+
+            dc = glob(path + "/" + args.model +  "/datacards/" + args.process + "_" + subop + "/*/datacard.txt")
+            fit = glob(path + "/" + args.model +  "/datacards/" + args.process + "_" + subop + "/*/higgs*.root")
+
+            if len(dc) != 1 or len(fit) != 1: sys.exit("[ERROR] too  many things in {}".format(path + "/" + args.model +  "/datacards/" + args.process + "_" + subop))
+            best_var = dc[0].split("/")[-2] #last is datacard.txt, before var1_var2_...
+
+            final_dict[subop]["combined"] = fit[0] #path to the combined fit
+
+            #now retrieving each channel fit
+
+            datacard = open(dc[0], 'r')
+            lines = datacard.readlines()
+
+            #all vars is a list of the type
+            # [['SSWW', 'ptl2'], ['OSWW', 'ptl2']] 
+            #1st entry is the channel name, second is the beest variable in the datacard
+            all_vars = []
+            for line in lines:
+                if "bin     " in line:
+                    ch_var = (line.split("bin     ")[1:])[0]
+                    ch_var = (ch_var.strip(" ")).split(" ")
+                    for i in ch_var:
+                        if i != '' and i != "\n":
+                            all_vars.append(i.strip("\n").split("_"))
+                    break
+
+            channels = [i[0] for i in all_vars]
+            bestv = [i[1] for i in all_vars]
+
+            if not all([i for i in channels for i in args.ch]): sys.exit("[ERROR] not all channels in datacards are specified in input")
+            
+            for channel, var in zip(channels, bestv):
+                # retrieve the fit
+                idx = chprocess_.index(channel)
+                ch_path = ch_[idx]
+                ch_prefix = chp_[idx]
+
+                subfit = glob(ch_path + "/" + ch_prefix + "_" + channel + "_" + subop + "/" + args.model +  "/datacards/" + channel + "_" + subop + "/{}/higgs*.root".format(var))
+
+                if len(subfit) != 1: sys.exit("[ERROR] Missing fit for channel in combination, searched at: {}".format(ch_path + "/" + ch_prefix + "_" + channel + "_" + subop + "/" + args.model +  "/datacards/" + channel + "_" + subop + "/{}/higgs*.root".format(var)))
+
+                final_dict[subop][channel] = subfit[0]
+
+
+    if args.verbose:
+        for key in final_dict.keys():
+            print("OP: " + key)
+            print("combined: " + final_dict[key]["combined"].split("/")[-2])
+
+            for ch in final_dict[key].keys():
+                if ch != "combined": 
+                    print("{}: {}".format(ch, final_dict[key][ch].split("/")[-2]))
+            print(" ")
+
+    
+    #Now writing everything into the config file
+    file_ = open(args.out, 'w')
+    file_.write("from collections import OrderedDict \n\n\n")
+    file_.write("operators = OrderedDict()\n\n\n")
+
+    file_.write("is_combo = True\n\n\n")
+
+    for sub in final_dict.keys():
+        file_.write("operators['{}'] = OrderedDict()\n".format(sub))
+
+        idx = 0
+        for key in final_dict[sub].keys():
+            if key != "combined":
+                file_.write("operators['{}']['{}']  = OrderedDict()\n".format(sub, key))
+                file_.write("operators['{}']['{}']['path'] =  '{}'\n".format(sub, key, final_dict[sub][key]))
+                file_.write("operators['{}']['{}']['color'] =  '{}'\n".format(sub, key, colors[idx]))
+                file_.write("operators['{}']['{}']['linestyle'] =  '{}'\n".format(sub, key, lines_[idx]))
+                idx += 1
+
+
+        comb_color = colors[-1]
+        comb_ls = lines_[-1]
+
+        file_.write("operators['{}']['combined']  = OrderedDict()\n".format(sub, 'combined'))
+        file_.write("operators['{}']['combined']['path'] =  '{}'\n".format(sub, final_dict[sub]['combined']))
+        file_.write("operators['{}']['combined']['color'] =  '{}'\n".format(sub, comb_color))
+        file_.write("operators['{}']['combined']['linestyle'] =  '{}'\n".format(sub, comb_ls))
+
+        file_.write("\n\n")
+
+
+    
+
+
