a88436eda9402a548f6836cc2d086797970f71b4
Giacomo Boldrini <gboldrin@cern.ch>
2021-04-30 16:48:07 +0200

    Updated to plot QCD stacked

--- paperPlot.py
+++ paperPlot.py
@@ -19,26 +19,32 @@ ceft2 = ROOT.TColor.GetFreeColorIndex()
 col4 = ROOT.TColor(ceft2, 0.98431372549, 0.54509803921, 0.14117647058)
 ceft3 = ROOT.TColor.GetFreeColorIndex()
 col5 = ROOT.TColor(ceft3, 0.39215686274, 0.55294117647, 0.89803921568)
+csmqcd = ROOT.TColor.GetFreeColorIndex()
+col6 = ROOT.TColor(csmqcd, 0.63921568627, 0.4862745098, 0.25098039215)
 
 def GetProcess(proc):
 
     d = {
-        'SSWW': "W^{#pm}W^{#pm}+2j (EWK)",
-        'OSWW': "W^{#pm}W^{#mp}+2j (EWK)",
-        'WZeu': "W^{#pm}Z+2j (EWK)",
+        'SSWW': "W^{#pm}W^{#pm}+2j",
+        'OSWW': "W^{#pm}W^{#mp}+2j",
+        'WZeu': "W^{#pm}Z+2j",
         'inWW': "W^{#pm}W^{#mp}+0j",
-        'WZQCD': "W^{#pm}Z+2j (QCD)",
-        'OSWWQCD': "W^{#pm}W^{#mp}+2j (QCD)",
-        'OSWW_OSWWQCD': "W^{#pm}W^{#mp}+2j (EWK + QCD)",
-        'WZ_WZQCD': "W^{#pm}Z+2j (EWK + QCD)",
+        'WZQCD': "W^{#pm}Z+2j",
+        'OSWWQCD': "W^{#pm}W^{#mp}+2j",
+        'OSWW_OSWWQCD': "W^{#pm}W^{#mp}+2j",
+        'WZ_WZQCD': "W^{#pm}Z+2j",
     }
 
-    return d[proc]
+    if proc in d.keys():
+        return d[proc]
+    else:
+        return proc
 
 def GetColor():
 
     d = {
         'sm': ROOT.kGray,
+        'smqcd': csmqcd,
         #'lin': ROOT.kRed-4,
         #'quad': ROOT.kGreen+2,
         #'BSM': [ROOT.kOrange-3, ROOT.kAzure+1, ROOT.kPink+6],
@@ -60,20 +66,23 @@ def convertName(name):
         "phij2" : "#phi_{j2}",
         "ptj1" : "p_{T,j1} [GeV]",
         "ptj2" : "p_{T,j2} [GeV]",
-        "ptl1" : "p_{T,l1} [GeV]",
-        "ptl2" : "p_{T,l2} [GeV]",
+        "ptl2" : "p_{T,l1} [GeV]",
+        "ptl1" : "p_{T,l2} [GeV]",
         "ptl3" : "p_{T,l3} [GeV]",
         "ptll" : "p_{T,ll} [GeV]",
         "ptee" : "p_{T,ee} [GeV]",
         "deltaetajj": "#Delta#eta_{jj}",
         "etaj1" : "#eta_{j1}",
         "etaj2" : "#eta_{j2}",
-        "etal1" : "#eta_{l1}",
-        "etal2" : "#eta_{l2}",
+        "etal2" : "#eta_{l1}",
+        "etal1" : "#eta_{l2}",
         " ": None
     }
 
-    return d[name]
+    if name in d.keys():
+        return d[name]
+    else:
+        return name
 
 def mkdir(path):
     try:
@@ -101,11 +110,16 @@ if __name__ == '__main__':
     parser.add_argument('--quadscale', dest='quadscale', help='Scale quadratic component in the plot', required=False, default = "1")
     parser.add_argument('--drawstat', dest='drawstat', help='Draw sedcond pair of plots with SM stat unc', required=False, action="store_true", default=False)
     parser.add_argument('--ymaxdigits', dest='ymaxdigits', help='Argument on y axis MaxDigits', required=False, default = "3")
-    parser.add_argument('--xposchannel', dest='xposchannel', help='xposition of the channel label on the upper left corner', required=False, default = "0.42")
+    parser.add_argument('--ymaxscale', dest='ymaxscale', help='Argument on y axis scale factor: y_max + scale*y_max default 0.5', required=False, default = "0.5")
+    parser.add_argument('--xposchannel', dest='xposchannel', help='xposition of the channel label on the upper left corner, default 0.3', required=False, default = "0.3")
+    parser.add_argument('--stackQCD', dest='stackQCD', help='Path to QCD Shapes. Will retrieve SM shapes and stack them with EWK SM', required=False, default = None )
+    parser.add_argument('--prefixQCD', dest='prefixQCD', help='Prefix of  the QCD folder', required=False, default = None )
+    parser.add_argument('--setLogY', dest='setLogY', help='Set logY on canvas. This may yield errors if linear is negative. Default is false', required=False, default = False, action="store_true" )
 
     args = parser.parse_args()
 
     ROOT.gROOT.SetBatch(1)
+    ROOT.TH1.SetDefaultSumw2(True)
 
     d = GetColor()
 
@@ -117,6 +131,8 @@ if __name__ == '__main__':
 
     mkdir(args.out)
 
+    os.system("cp /afs/cern.ch/user/g/gboldrin/index.php " + args.out)
+
     lin_scale = float(args.linscale)
     quad_scale = float(args.quadscale)
     model = args.model
@@ -127,12 +143,29 @@ if __name__ == '__main__':
 
     xpos_proc = float(args.xposchannel)
 
+
+    if args.stackQCD != None:
+        if args.prefixQCD == None: sys.exit("[ERROR] Specify the QCD folder prefix like to_Latinos_OSWWQCD")
+        qcdsub = glob(args.stackQCD + "/" + args.prefixQCD + "*")
+        qcdsm = {}
+        qcdops = [i.split("/")[-1].split(args.prefixQCD + "_")[1] for i in qcdsub]
+        for folder, op_ in zip(qcdsub, qcdops):
+            qcdsm[op_] = {}
+            f = ROOT.TFile(folder + "/" + args.model + "/rootFile/histos.root")
+            proc_op = [i.GetName() for i in f.GetListOfKeys()][0]
+            proc = proc_op.split("_" + op_)[0]
+            dict_ = f.Get(proc_op) # this is related to the file structure after mkDCInput.py
+            variables = [i.GetName() for i in dict_.GetListOfKeys()]
+            for var in variables:
+                qcdsm[op_][var] = deepcopy( f.Get(proc_op + "/" + var + "/histo_sm") )
+
+            f.Close()
+
     #suppose one single shape file for variable folder:
 
     for folder, op in zip(subfs, ops):
         
         variables = glob(folder + "/" + model + "/datacards/*/*")
-
         the_process = variables[0].split("/")[-2].split("_{}".format(op))[0]
 
         vars_ = [i.split("/")[-1] for i in variables]
@@ -142,8 +175,6 @@ if __name__ == '__main__':
             #retireve shape:
 
             shape_file = glob(var_f + "/shapes/*.root")[0]
-            print(shape_file)
-
             f = ROOT.TFile(shape_file)
 
             if model == "EFT":
@@ -166,26 +197,6 @@ if __name__ == '__main__':
                 h_lin.Add(h_sm_neg)
                 h_lin.Add(h_quad_neg)
 
-            h_bsm = []
-
-            for i in ks:
-                h_bsm.append(deepcopy(h_sm))
-
-            for i,j in enumerate(ks):
-                h_l = deepcopy(h_lin)
-                h_l.Scale(j)
-
-                h_q = deepcopy(h_quad)
-                h_q.Scale(j*j)
-
-                h_bsm[i].Add(h_l)
-                h_bsm[i].Add(h_q)
-                #print("k = {}, bsm min: {}".format(j, h_bsm[i].GetMinimum()))
-
-                h_lin.Scale(lin_scale)
-                h_quad.Scale(quad_scale)
-
-            
 
             #plotting
             ROOT.gStyle.SetOptStat(0)
@@ -210,7 +221,7 @@ if __name__ == '__main__':
             pad1.cd()
             h_sm.SetFillColor(d["sm"])
             h_sm.SetLineColor(0)
-
+                
             #take as inf the minimum lin. If > 0 then min = 0
 
             y_min = h_lin.GetMinimum()
@@ -226,10 +237,45 @@ if __name__ == '__main__':
             h_sm.GetXaxis().SetTitleOffset(1.2)
             h_sm.GetYaxis().SetTitle("Events")
             h_sm.GetYaxis().SetTitleSize(0.05)
-            h_sm.GetYaxis().SetLabelSize(0.05)
+            h_sm.GetYaxis().SetLabelSize(0.04)
             h_sm.GetYaxis().SetTitleOffset(1)
             h_sm.GetYaxis().SetMaxDigits(maxd)
-            h_sm.Draw("hist")
+            #h_sm.Draw("hist")
+
+            if args.stackQCD != None and op in qcdsm.keys():
+                h_smstack = ROOT.THStack("hs", ";;Events")
+                h_smqcd = qcdsm[op][var_n]
+                h_smewk = deepcopy(h_sm)
+                h_smewk.Add(h_smqcd,-1)
+                h_smqcd.SetFillColor(ROOT.kGray + 1)
+                h_smqcd.SetLineColor(ROOT.kGray + 1)
+                h_smstack.Add(h_smqcd)
+                h_smstack.Add(h_smewk)
+                h_smstack.Draw("hist")
+                h_smstack.GetYaxis().SetTitleSize(0.05)
+                h_smstack.GetYaxis().SetLabelSize(0.04)
+                h_smstack.GetYaxis().SetTitleOffset(1)
+                h_smstack.GetYaxis().SetMaxDigits(maxd)
+                c.Update()
+                
+
+            elif args.stackQCD != None and op not in qcdsm.keys():
+                h_smstack = ROOT.THStack("hs", ";;Events")
+                h_smqcd = qcdsm[qcdsm.keys()[0]][var_n]
+                h_smewk = deepcopy(h_sm)
+                h_smqcd.SetFillColor(ROOT.kGray + 1)
+                h_smqcd.SetLineColor(ROOT.kGray + 1)
+                h_smstack.Add(h_smqcd)
+                h_smstack.Add(h_smewk)
+                h_smstack.Draw("hist")
+                h_smstack.GetYaxis().SetTitleSize(0.05)
+                h_smstack.GetYaxis().SetLabelSize(0.04)
+                h_smstack.GetYaxis().SetTitleOffset(1)
+                h_smstack.GetYaxis().SetMaxDigits(maxd)
+                c.Update()
+            
+            else:
+                h_sm.Draw("hist")
 
             if h_quad.GetMaximum() > y_max: y_max = h_quad.GetMaximum()
 
@@ -245,24 +291,67 @@ if __name__ == '__main__':
             h_lin.SetLineStyle(7)
             h_lin.Draw("hist same")
 
+            h_bsm = []
+
+            for i in ks:
+                if args.stackQCD == None:
+                    h_temp = deepcopy(h_sm)
+                    h_temp.SetFillColor(0)
+                    h_bsm.append(deepcopy(h_temp))
+                else:
+                    if op in qcdsm.keys():
+                        h_temp = deepcopy(h_sm)
+                        h_temp.SetFillColor(0)
+                        h_bsm.append(h_temp)
+
+                    else:
+                        h_temp = deepcopy(h_smewk)
+                        h_temp.SetFillColor(0)
+                        h_temp.Add(h_smqcd)
+                        h_bsm.append(h_temp)
+
+            for i,j in enumerate(ks):
+                h_l = deepcopy(h_lin)
+                h_l.Scale(j)
+
+                h_q = deepcopy(h_quad)
+                h_q.Scale(j*j)
+
+                h_bsm[i].Add(h_l)
+                h_bsm[i].Add(h_q)
+                #print("k = {}, bsm min: {}".format(j, h_bsm[i].GetMinimum()))
+
+                h_lin.Scale(lin_scale)
+                h_quad.Scale(quad_scale)
+
+
             for i,j in enumerate(h_bsm):
                 if j.GetMaximum() > y_max: y_max = j.GetMaximum()
                 j.SetLineColor(d["BSM"][i])
                 j.SetLineWidth(3)
                 j.Draw("hist same")
 
-            h_sm.GetYaxis().SetRangeUser(y_min, y_max + 0.4*y_max)
+            if args.stackQCD != None: 
+                h_smstack.SetMinimum(y_min)
+                h_smstack.SetMaximum(y_max + float(args.ymaxscale)*y_max)
+            else:
+                h_sm.GetYaxis().SetRangeUser(y_min, y_max + float(args.ymaxscale)*y_max)
+
             c.Update()
             
             if legpos == 1:
-                leg = ROOT.TLegend(0.15, 0.55, 0.49, 0.86)
+                leg = ROOT.TLegend(0.15, 0.55, 0.54, 0.86)
             elif legpos == 2:
-                leg = ROOT.TLegend(0.55, 0.5, 0.89, 0.86)
+                leg = ROOT.TLegend(0.5, 0.5, 0.89, 0.86)
             leg.SetBorderSize(0)
             leg.SetNColumns(1)
             leg.SetTextSize(0.04)
 
-            leg.AddEntry(h_sm, "SM", "F")
+            if args.stackQCD != None:
+                leg.AddEntry(h_smewk, GetProcess(the_process) + " EWK", "F")
+                leg.AddEntry(h_smqcd, GetProcess(the_process) + " QCD", "F")
+            else:
+                leg.AddEntry(h_sm, GetProcess(the_process), "F")
 
             if quad_scale != 1:
                 leg.AddEntry(h_quad, "Quad {} #times {}".format(op, quad_scale), "F")
@@ -280,11 +369,11 @@ if __name__ == '__main__':
             leg.Draw()
             
             #Fancy stuffs
-            tex3 = ROOT.TLatex(0.90,.915,"100 fb^{-1}   (13 TeV)")
+            tex3 = ROOT.TLatex(0.90,.915,"100 fb^{-1} (13 TeV)")
             tex3.SetNDC()
             tex3.SetTextAlign(31)
             tex3.SetTextFont(42)
-            tex3.SetTextSize(0.06)
+            tex3.SetTextSize(0.05)
             tex3.SetLineWidth(3)
             tex3.Draw()
 
@@ -293,7 +382,7 @@ if __name__ == '__main__':
             tex4.SetNDC()
             tex4.SetTextAlign(31)
             tex4.SetTextFont(42)
-            tex4.SetTextSize(0.06)
+            tex4.SetTextSize(0.05)
             tex4.SetLineWidth(3)
             tex4.Draw()
 
@@ -302,7 +391,12 @@ if __name__ == '__main__':
             h_bsm_r = []
 
             for i,j in enumerate(ks):
-                h_ = deepcopy(h_sm)
+                if args.stackQCD == None:
+                    h_ = deepcopy(h_sm)
+                else:
+                    h_ = deepcopy(h_smewk)
+                    h_.Add(h_smqcd)
+
                 h_.Divide(h_bsm[i])
 
                 h_.SetFillColor(0)
@@ -325,8 +419,6 @@ if __name__ == '__main__':
             h_bsm_r[0].Draw("hist")
             for i in h_bsm_r[1:]: i.Draw("hist same")
 
-
-
             l = ROOT.TLine(h_sm.GetXaxis().GetXmin(), 1, h_sm.GetXaxis().GetXmax(), 1)
             l.SetLineColor(ROOT.kGray+1)
             l.SetLineWidth(2)
@@ -337,6 +429,21 @@ if __name__ == '__main__':
             c.Print(args.out + "/" + op + "_" + var_n + ".pdf")
             c.Print(args.out + "/" + op + "_" + var_n + ".png")
             if args.root: c.Print(args.out + "/" + op + "_" + var_n + ".root")
+
+            if args.setLogY:
+                pad1.cd()
+                if args.stackQCD != None: 
+                    h_smstack.SetMinimum(1)
+                    h_smstack.SetMaximum(100*h_smstack.GetMaximum())
+                else:
+                    h_sm.SetMinimum(1)
+                    h_sm.SetMaximum(100*h_sm.GetMaximum())
+
+                c.Update()
+                pad1.SetLogy(True)
+
+                c.Print(args.out + "/" + op + "_" + var_n + "_log.pdf")
+                c.Print(args.out + "/" + op + "_" + var_n + "_log.png")
 
 
             #Now draw the same but with uncertainties on the sm
@@ -361,13 +468,26 @@ if __name__ == '__main__':
 
                 pad3.cd()
 
-                h_err = deepcopy(h_sm)
-                h_err.Sumw2()
+                if args.stackQCD != None: 
+                    h_smstack.SetMinimum(y_min)
+                    h_smstack.SetMaximum(y_max + float(args.ymaxscale)*y_max)
+                else:
+                    h_sm.GetYaxis().SetRangeUser(y_min, y_max + float(args.ymaxscale)*y_max)
+
+                if args.stackQCD == None:
+                    h_err = deepcopy(h_sm)
+                else:
+                    h_err = deepcopy(h_smewk)
+                    h_err.Add(h_smqcd)
+
                 h_err.SetMarkerSize(0)
                 h_err.SetFillColor(ROOT.kBlack)
                 h_err.SetFillStyle(3004)
 
-                h_sm.Draw("hist")
+                if args.stackQCD == None:
+                    h_sm.Draw("hist")
+                else:
+                    h_smstack.Draw("hist")
                 h_quad.Draw("hist same")
                 h_lin.Draw("hist same")
                 h_err.Draw("E2 same")
@@ -376,14 +496,19 @@ if __name__ == '__main__':
                     j.Draw("hist same")
                 
                 if legpos == 1:
-                    leg1 = ROOT.TLegend(0.15, 0.51, 0.49, 0.86)
+                    leg1 = ROOT.TLegend(0.15, 0.5, 0.54, 0.86)
                 elif legpos == 2:
-                    leg1 = ROOT.TLegend(0.55, 0.5, 0.89, 0.86)
+                    leg1 = ROOT.TLegend(0.5, 0.5, 0.89, 0.86)
                 leg1.SetBorderSize(0)
                 leg1.SetNColumns(1)
                 leg1.SetTextSize(0.04)
 
-                leg1.AddEntry(h_sm, "SM", "F")
+                if args.stackQCD == None:
+                    leg1.AddEntry(h_sm, GetProcess(the_process), "F")
+                else:
+                    leg1.AddEntry(h_smewk, GetProcess(the_process) + " EWK", "F")
+                    leg1.AddEntry(h_smqcd, GetProcess(the_process) + " QCD", "F")
+
                 leg1.AddEntry(h_err, "SM Stat. Unc.", "F")
 
                 if quad_scale != 1:
@@ -402,11 +527,11 @@ if __name__ == '__main__':
                 leg1.Draw()
 
                 #Fancy stuffs
-                tex5 = ROOT.TLatex(0.90,.915,"100 fb^{-1}   (13 TeV)")
+                tex5 = ROOT.TLatex(0.90,.915,"100 fb^{-1} (13 TeV)")
                 tex5.SetNDC()
                 tex5.SetTextAlign(31)
                 tex5.SetTextFont(42)
-                tex5.SetTextSize(0.06)
+                tex5.SetTextSize(0.05)
                 tex5.SetLineWidth(3)
                 tex5.Draw()
 
@@ -415,15 +540,19 @@ if __name__ == '__main__':
                 tex6.SetNDC()
                 tex6.SetTextAlign(31)
                 tex6.SetTextFont(42)
-                tex6.SetTextSize(0.06)
+                tex6.SetTextSize(0.05)
                 tex6.SetLineWidth(3)
                 tex6.Draw()
 
                 pad4.cd()
 
-                h_bsm_ratio_err = deepcopy(h_sm)
+                if args.stackQCD == None:
+                    h_bsm_ratio_err = deepcopy(h_sm)
+                else:
+                    h_bsm_ratio_err = deepcopy(h_smewk)
+                    h_bsm_ratio_err.Add(h_smqcd)
+
                 h_bsm_ratio_err.Divide(h_sm)
-                h_bsm_ratio_err.Sumw2()
                 h_bsm_ratio_err.SetFillColor(ROOT.kBlack)
                 h_bsm_ratio_err.SetFillStyle(3004)
 
@@ -471,6 +600,11 @@ if __name__ == '__main__':
                     i.Draw("P same")
                     i.Draw("hist same")
 
+                if args.setLogY:
+                    try:
+                        c1.SetLogy()
+                    except:
+                        pass
 
                 c1.Update()
                 c1.Draw()
@@ -478,7 +612,19 @@ if __name__ == '__main__':
                 c1.SaveAs(args.out + "/" + op + "_" + var_n + "_stat.png")
                 if args.root: c1.SaveAs(args.out + "/" + op + "_" + var_n + "_stat.root")
 
-            #finally copy the index in here:
-            os.system("cp /afs/cern.ch/user/g/gboldrin/index.php " + args.out)
+                if args.setLogY:
+                    pad3.cd()
+                    if args.stackQCD != None: 
+                        h_smstack.SetMinimum(1)
+                        h_smstack.SetMaximum(100*h_smstack.GetMaximum())
+                    else:
+                        h_sm.SetMinimum(1)
+                        h_sm.SetMaximum(100*h_sm.GetMaximum())
+
+                    c1.Update()
+                    pad3.SetLogy(True)
+
+                    c1.Print(args.out + "/" + op + "_" + var_n + "_stat_log.pdf")
+                    c1.Print(args.out + "/" + op + "_" + var_n + "_stat_log.png")
 
             f.Close()
